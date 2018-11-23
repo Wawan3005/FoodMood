1d0abf59ff630bde9210e4771ebca6991db19316
JHPCGit <hugo.jeanty@orange.fr>
2018-11-23 14:55:07 +0100

    Mon message à moi2

--- app/src/main/java/com/example/wawan/foodmood/MainActivity.java
+++ app/src/main/java/com/example/wawan/foodmood/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // mon medfnefrnj,kf
+        // mon medfnefrnj,kfgh
         setContentView(R.layout.activity_main);
     }
 }
